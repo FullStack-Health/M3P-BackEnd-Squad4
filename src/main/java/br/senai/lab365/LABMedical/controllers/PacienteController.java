7543091a6e72709a45e2994e5a1a0e1a08cf3bcd
André <andrejusmattos@gmail.com>
2024-07-23 09:42:26 -0300

    primeiro commit

---
+++ src/main/java/br/senai/lab365/LABMedical/controllers/PacienteController.java
@@ -0,0 +1,12 @@
+package br.senai.lab365.LABMedical.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pacientes")
+public class PacienteController {
+
+
+}
