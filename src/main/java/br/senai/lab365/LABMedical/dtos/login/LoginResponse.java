682d33ec9211284cb7747a4d172ab47ebb1b31e7
André <andrejusmattos@gmail.com>
2024-11-03 22:52:22 -0300

    Adição de parâmetros ao LoginResponse

--- src/main/java/br/senai/lab365/LABMedical/dtos/login/LoginResponse.java
+++ src/main/java/br/senai/lab365/LABMedical/dtos/login/LoginResponse.java
@@ -2,5 +2,12 @@ package br.senai.lab365.LABMedical.dtos.login;
 
 import java.util.List;
 
-public record LoginResponse(String token, Long tempoExpiracao, List<String> listaNomesPerfis, String pacienteId) {
-}
+public record LoginResponse(
+        String token,
+        Long tempoExpiracao,
+        List<String> listaNomesPerfis,
+        String pacienteId,
+        String usuarioId,
+        String email,
+        String nome
+) {}
